6bccfa0fbba20f02c6eeda499681ca2d746f5f75
jezzaking <jeremyking77+git@gmail.com>
2015-01-14 09:54:12 +0000

    with grid working

--- HelloWorldMVC/Controllers/HomeController.cs
+++ HelloWorldMVC/Controllers/HomeController.cs
@@ -1,14 +1,27 @@
-﻿using System;
+﻿using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-
 namespace HelloWorldMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private static AdventureWorksEntities e;
+
+        public HomeController()
+        {
+            e = new AdventureWorksEntities();
+        }
+
+        ~HomeController()
+        {
+            e.Dispose();
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
@@ -34,18 +47,27 @@ namespace HelloWorldMVC.Controllers
         public ActionResult MyView()
         {
             
-            string customer;
-            using (AdventureWorksEntities e = new AdventureWorksEntities())
-            {
-                customer = (from cust in e.SalesOrderHeader
-                           select cust.CustomerID).FirstOrDefault().ToString();
-
-            }
-
-            ViewBag.Message = customer;
-
-
+            ViewBag.Message = "";
             return View();
         }
+
+        public ActionResult TestGrid()
+        {
+            ViewBag.Message = "People List";
+            return View();
+        }
+
+        public ActionResult Customers_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            return Json(GetCustomers().ToDataSourceResult(request));
+        }
+
+        private static IEnumerable<Person> GetCustomers()
+        {
+            var customer = (from cust in e.Person
+                        select cust).Take(50);
+            return customer.AsEnumerable(); 
+        }
+
     }
 }
